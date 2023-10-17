1e5c618053527155894596841dcd695b0d114f11
Jean Eric TUYISHIMIRE <ericjohn415@gmail.com>
2023-10-17 03:50:02 +0200

    minor update

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
   res.writeHead(200);
-  res.end("Hello world TUYISHIMIRE!");
+  res.end("Hello world Jean Eric!");
 });
 server.listen(3000);
